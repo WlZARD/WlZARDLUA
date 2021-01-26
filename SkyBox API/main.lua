bb69681dc524eae5b9ae25178a2dd6397b3c6dcd
Jason <78008106+WlZARD@users.noreply.github.com>
2021-01-26 05:13:44 +0000

    Update main.lua

--- SkyBox API/main.lua
+++ SkyBox API/main.lua
@@ -28,3 +28,57 @@ if Skybox == "Alien Planet" then
     Sky.SkyboxRt = "rbxassetid://153962645"
     Sky.SkyboxUp = "rbxassetid://153962699"
 end
+
+if Skybox == "Tropical Island" then
+    Sky.SkyboxBk = "rbxassetid://169210090"
+    Sky.SkyboxDn = "rbxassetid://169210108"
+    Sky.SkyboxFt = "rbxassetid://169210121"
+    Sky.SkyboxLf = "rbxassetid://169210133"
+    Sky.SkyboxRt = "rbxassetid://169210143"
+    Sky.SkyboxUp = "rbxassetid://169210149"
+end
+
+if Skybox == "Green Screen" then
+    Sky.SkyboxBk = "rbxassetid://61831385"
+    Sky.SkyboxDn = "rbxassetid://61831385"
+    Sky.SkyboxFt = "rbxassetid://61831385"
+    Sky.SkyboxLf = "rbxassetid://61831385"
+    Sky.SkyboxRt = "rbxassetid://61831385"
+    Sky.SkyboxUp = "rbxassetid://61831385"
+end
+
+if Skybox == "Clear Sky" then
+    Sky.SkyboxBk = "rbxassetid://98202165"
+    Sky.SkyboxDn = "rbxassetid://98202176"
+    Sky.SkyboxFt = "rbxassetid://98202152"
+    Sky.SkyboxLf = "rbxassetid://98202161"
+    Sky.SkyboxRt = "rbxassetid://98202146"
+    Sky.SkyboxUp = "rbxassetid://98202169"
+end
+
+if Skybox == "Dark Sky" then
+    Sky.SkyboxBk = "rbxassetid://48020371"
+    Sky.SkyboxDn = "rbxassetid://48020144"
+    Sky.SkyboxFt = "rbxassetid://48020234"
+    Sky.SkyboxLf = "rbxassetid://48020211"
+    Sky.SkyboxRt = "rbxassetid://48020254"
+    Sky.SkyboxUp = "rbxassetid://48020383"
+end
+
+if Skybox == "Mountain Sky" then
+    Sky.SkyboxBk = "rbxassetid://35268852"
+    Sky.SkyboxDn = "rbxassetid://35268837"
+    Sky.SkyboxFt = "rbxassetid://35268884"
+    Sky.SkyboxLf = "rbxassetid://35268868"
+    Sky.SkyboxRt = "rbxassetid://35268872"
+    Sky.SkyboxUp = "rbxassetid://35268877"
+end
+
+if Skybox == "Troll Sky" then
+    Sky.SkyboxBk = "rbxassetid://53198821"
+    Sky.SkyboxDn = "rbxassetid://53198821"
+    Sky.SkyboxFt = "rbxassetid://53198821"
+    Sky.SkyboxLf = "rbxassetid://53198821"
+    Sky.SkyboxRt = "rbxassetid://53198821"
+    Sky.SkyboxUp = "rbxassetid://53198821"
+end
