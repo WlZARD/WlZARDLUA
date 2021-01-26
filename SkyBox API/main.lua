7d100a2e22167309e60ac90fbd93fe37de96d755
Jason <78008106+WlZARD@users.noreply.github.com>
2021-01-26 05:02:35 +0000

    Update main.lua

--- SkyBox API/main.lua
+++ SkyBox API/main.lua
@@ -1,4 +1,6 @@
-local Sky = Instance.new("Sky", game.Lighting)
+local Lighting = game.Lighting
+local Sky = Instance.new("Sky", Lighting)
+Sky.Name = "CustomSky"
 
 if Skybox == "Alien Purple" then
     Sky.SkyboxBk = "rbxassetid://2356676179"
@@ -16,4 +18,13 @@ if Skybox == "Alien" then
     Sky.SkyboxLf = "rbxassetid://1012889"
     Sky.SkyboxRt = "rbxassetid://1012888"
     Sky.SkyboxUp = "rbxassetid://1014449"
+end
+
+if Skybox == "Alien Planet" then
+    Sky.SkyboxBk = "rbxassetid://153962652"
+    Sky.SkyboxDn = "rbxasset://textures/sky/sky512_dn.tex"
+    Sky.SkyboxFt = "rbxassetid://153962663"
+    Sky.SkyboxLf = "rbxassetid://153962636"
+    Sky.SkyboxRt = "rbxassetid://153962645"
+    Sky.SkyboxUp = "rbxassetid://153962699"
 end
