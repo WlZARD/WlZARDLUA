d3db90030f09b67a9919835ac338fe39764bb108
Jason <78008106+WlZARD@users.noreply.github.com>
2021-01-26 04:38:34 +0000

    Update main.lua

--- SkyBox API/main.lua
+++ SkyBox API/main.lua
@@ -8,3 +8,12 @@ if Skybox == "Alien Purple" then
     Sky.SkyboxRt = "rbxassetid://2356672936"
     Sky.SkyboxUp = "rbxassetid://2356671994"
 end
+
+if Skybox == "Alien" then
+    Sky.SkyboxBk = "http://www.roblox.com/asset/?version=1&id=1012890"
+    Sky.SkyboxDn = "http://www.roblox.com/asset/?version=1&id=1012891"
+    Sky.SkyboxFt = "http://www.roblox.com/asset/?version=1&id=1012887"
+    Sky.SkyboxLf = "http://www.roblox.com/asset/?version=1&id=1012889"
+    Sky.SkyboxRt = "http://www.roblox.com/asset/?version=1&id=1012888"
+    Sky.SkyboxUp = "http://www.roblox.com/asset/?version=1&id=1014449"
+end
